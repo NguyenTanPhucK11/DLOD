aedd0ae5484e7d7e7b2918c1b69d0d6adfcd3952
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2021-01-08 02:42:35 +0700

    call api cuisine

--- actions/actions.py
+++ actions/actions.py
@@ -11,7 +11,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-
+import json
 import requests
 
 
@@ -33,27 +33,53 @@ class ActionGetWeather(Action):
             temp = round(response.json()["main"]["temp"])
             cityGR = response.json()["name"]
 
-            msg = f"Nhiệt độ hiện tại tại {cityGR} là {temp} độ C và {description}"
+            msg = f"Nhiệt độ hiện tại tại {city} là {temp} độ C và {description}"
         else:
             msg= "Lỗi!"
 
         dispatcher.utter_message(msg)
         return [SlotSet("location", None)]
+class GetName(Action):
+    def name(self):
+	    return 'action_name'
+		
+    def run(self, dispatcher, tracker, domain):
+		
+        most_recent_state = tracker.current_state()
+        sender_id = most_recent_state['sender_id']
 
+        r = requests.get('https://graph.facebook.com/{}?fields=first_name,last_name,profile_pic&access_token={}'.format(sender_id, fb_access_token)).json()
+        first_name = r['first_name']
+        last_name = r['last_name']
+        print(first_name)
+        return [SlotSet('name', first_name), SlotSet('surname', last_name)]
 class ActionTest(Action):
     
     def name(self):
         return "action_api"
 
     def run(self, dispatcher, tracker, domain):
-        response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+        response = requests.get("http://192.168.100.44:3000/place-travels")
         jsonResponse = response.json()
-        title = jsonResponse["title"]
-        id = jsonResponse["id"]
+        # print(jsonResponse)
+        # listPlaceTravel = map(lambda item: item.name, jsonResponse)
+        
+        # print(listPlaceTravel)
+        # print(list(listPlaceTravel))
+        # json_string = json.dumps(jsonResponse)
+        # print(json_string)
+        # test_list = [1, 4, 5, 6, 7]
+        for index, value in enumerate(jsonResponse):
+            name = value["name"]
+            type = value["type"]
+            review = value["review"]
+            messageToUser = "{}. {} \n {} \n ⭐⭐⭐⭐{}\n".format(index +1 ,name, type, review)
+            dispatcher.utter_message(messageToUser)
+        # title = jsonResponse[0]["name"]
+        # id = jsonResponse[0]["id"]
 
-        messageToUser = "id là \"{}\" và title là \"{}\"".format(id, title)
-        dispatcher.utter_message(messageToUser)
+        # messageToUser = "id2 là \"{}\" và title là \"{}\"".format(id, title)
+        # dispatcher.utter_message(messageToUser)
 
         return []
-
     
